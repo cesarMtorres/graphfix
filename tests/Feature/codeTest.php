aa88c5e4f2d1dd3b25928c0e9244c17d276156e5
cesar molina <cmolina@promarketingchile.com>
2025-09-13 12:21:24 -0500

    install api

---
+++ tests/Feature/codeTest.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @test
+ */
+test('can run isolated code', function () {
+
+    $response = $this->postJson('api/run-php', [
+        'code' => '<?php echo "Hola Mundo!";'
+    ]);
+    $response->assertStatus(200)
+        ->assertJson([
+            'status' => true,
+            'output' => 'Hola Mundo!'
+        ]);
+});
