9a7f187b53ffc0878846e8c8bd6cfbc14adbd0b5
cesar molina <cmolina@promarketingchile.com>
2025-09-13 08:18:51 -0500

    CHORE: EDITOR BASE

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
-
-Route::get('/', function () {
-    return Inertia::render('welcome');
-})->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('dashboard', function () {
@@ -13,5 +10,8 @@ Route::middleware(['auth', 'verified'])->group(function () {
     })->name('dashboard');
 });
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
